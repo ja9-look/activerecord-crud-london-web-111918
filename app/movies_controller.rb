4eac3216c77c3e876199ba80b39aa5efb0c62059
Janine Luk <janineluk95@gmail.com>
2018-12-02 16:15:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,14 +1,11 @@
 # Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
+
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.create
   movie.title = "This is a title."
-  __
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,7 +17,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.find_by
 end
 
 def can_be_created_in_a_block(args = __)
